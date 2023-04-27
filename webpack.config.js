09e0ec488dd8b2132e553bbf2668950d79f9f8d2
aqutw <aquarian.ex@gmail.com>
2023-04-27 21:06:52 +0800

    starting webpack.config.js w ENV

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+let MODE = process.env.MODE;
+console.log('MODE :', MODE);
+if (!['production', 'development'].includes(MODE)) {
+  MODE = 'production'
+}
+
+
+
+module.exports = {
+  mode: MODE,
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  }
+}
